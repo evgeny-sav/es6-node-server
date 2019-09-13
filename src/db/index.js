4962848766f525bdc280739a7a5dd80aa9716c8e
Yauheni Sauchanka <Yauheni_Sauchanka@epam.com>
2019-09-13 15:07:13 +0300

    initial commit

---
+++ src/db/index.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+import logger from '../utils/logger';
+import dotenv from 'dotenv';
+dotenv.config();
+
+import UserModel from './models/user';
+import MessageModel from './models/message';
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
+
+const connection = mongoose.connection;
+connection.on('error', err => logger.error(err));
+connection.once('open', () => logger.info('Connected to MongoDB'));
+
+export default {
+  UserModel,
+  MessageModel,
+};
